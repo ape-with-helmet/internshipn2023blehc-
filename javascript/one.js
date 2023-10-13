889fee0cc29881f7754edac5708c9a0754d34b9d
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-10-13 15:12:15 +0530

    sdsd

---
+++ javascript/one.js
@@ -0,0 +1,9 @@
+// alert("whatchu doing?");
+// prompt("Enter the name","");
+console.log("Shitty day init?");
+document.write("<br><h1>Java script is a language</h1>");
+var num = 54;
+var num=0;
+let num2=90;
+num2=89;
+console.log(num);
