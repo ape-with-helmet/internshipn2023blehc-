06334ca0661e4f7aca6b10e6241baa2de4e873aa
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-10-13 22:01:52 +0530

    plspls

--- javascript/calculator/calc.js
+++ javascript/calculator/calc.js
@@ -1,16 +1,23 @@
 function calculator(){
-    let n1 = document.querySelector("#num1").value;
-    let oper=document.querySelector("#op").value;
-    let n2=document.querySelector("#num2").value;
-    switch(oper){
-        case 'mul':
-            document.getElementById("ans")=parseFloat(n1)*parseFloat(n2);
-            break;
-        case 'add':
-            document.getElementById("ans")=parseFloat(n1)+parseFloat(n2);
-            break;
-        default:
-            alert("error");
-            break;
+    let n1 = document.querySelector('#num1').value;
+    let oper=document.querySelector('#op').value;
+    let n2=document.querySelector('#num2').value;
+    console.log(oper);
+    console.log(n1);
+    console.log(n2);
+    if (oper=='*') {
+        document.getElementById("ans").innerHTML=parseFloat(n1)*parseFloat(n2);
+    }
+    if (oper=='+') {
+        document.getElementById("ans").innerHTML=parseFloat(n1)+parseFloat(n2);
+    }
+    if (oper=='-') {
+        document.getElementById("ans").innerHTML=parseFloat(n1)-parseFloat(n2);
+    }
+    if (oper=='/') {
+        document.getElementById("ans").innerHTML=parseFloat(n1)/parseFloat(n2);
+    }
+    if (oper=='%') {
+        document.getElementById("ans").innerHTML=parseFloat(n1)%parseFloat(n2);
     }
 }
