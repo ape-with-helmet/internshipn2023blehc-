9fd24b76e48da45b5ce73660d9a2e71b76d7e233
Sadhguna Aithal <ganeshshatrugna@gmail.com>
2023-10-31 11:28:18 +0530

    ss

--- node-js/src/controller/studentControl.js
+++ node-js/src/controller/studentControl.js
@@ -5,30 +5,34 @@ const createStudent = async function(req,res){
     try{
         let student = req.body;
         let {fname,lname,email,password,mobile,address}=student;
-        if (!fname || !lname || !email || !password || !address || !mobile || !address){
+        if (!fname || !lname || !email || !password || !address || !mobile){
             return res.send({message : "Please provide all information !"})
         }
-        if (!validEmail) {
-            return res.send({message : "please enter valid email: "})
-        }
-        if (!validPassword) {
-            return res.send({message : "please enter valid password: "})
-        }
-        if (!validMobile) {
-            return res.send({message : "please enter valid mobile number: "})
-        }
+        // let valEmail = validEmail(email);
+        // if (!valEmail) {
+        //     return res.status(500).send({message : "please enter valid email: "})
+        // }
+        // let valPass = validPassword(password);
+        // if (!valPass) {
+        //     return res.status(500).send({message : "please enter valid password: "})
+        // }
+        // let valMob = validMobile(mobile);
+        // if (!valMob) {
+        //     return res.status(500).send({message : "please enter valid mobile number: "})
+        // }
+        console.log(validEmail(email));
         
-        //unique validation
-        let uniqueEmail = await studentModel.findOne({email})
-        if (uniqueEmail) {
-            return res.send({message : "Email already exists!"})
-        }
-        let uniquePhone = await studentModel.findOne({mobile})
-        if (uniquePhone) {
-            return res.send({message : "Phone already exists!"})
-        }
-        let createStu = await studentModel.create(student)
-        return res.send({message : createStu})
+        // //unique validation
+        // let uniqueEmail = await studentModel.findOne({email})
+        // if (uniqueEmail) {
+        //     return res.send({message : "Email already exists!"})
+        // }
+        // let uniquePhone = await studentModel.findOne({mobile})
+        // if (uniquePhone) {
+        //     return res.send({message : "Phone already exists!"})
+        // }
+        // let createStu = await studentModel.create(student)
+        // return res.send({message : createStu})
     }catch(err){
         console.log(err);
     }
@@ -60,12 +64,19 @@ let loginStudent = async (req,res)=>{
         let data = req.body;
         let {email,password}=data;
         if (!email || !password){
-            return res.send({message : "Please provide all information !"})
+            return res.status(400).send({message : "Please provide all information !"})
         }
         let matchStudent = await studentModel.findOne({email,password})
-        if (matchStudent) {
-            return res.status(200).send({message:"Student Registered"})//200 is the error code for "Working".
+        if (!matchStudent) {
+            return res.status(200).send({message:"Student not Registered"})//200 is the error code for "Working".
         }
+        const token = jwt.sign({
+            studentId:matchStudent._id.toString(),
+        },mernStack,
+        {
+            expiresIn:"12000sec"
+        })
+        return res.status(200).send({status: true, message: "Student Logged in Successfully",data:token})
 
     } catch (error) {
         return res.status(500).send({status:false, message:"Internal Server error!"})
--- node-js/src/validations/valid.js
+++ node-js/src/validations/valid.js
@@ -1,40 +1,28 @@
-function validEmail (email) {
+const validEmail = (email) => {
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (email.value.match(validRegex)) {
-        // alert("Valid email address!");
-        // document.form1.text1.focus();
         return true;
     } else {
-        // alert("Invalid email address!");
-        // document.form1.text1.focus();
         return false;
     }
 }
 
 //password validation
-function validPassword(password){
+const validPassword = (password) => {
     var validRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
     if (password.value.match(validRegex)) {
-        // alert("Valid email address!");
-        // document.form1.text1.focus();
         return true;
     } else {
-        // alert("Invalid email address!");
-        // document.form1.text1.focus();
         return false;
     }
 }
 
 //mobile validation
-function validMobile(mobile){
+const validMobile = (mobile) => {
     var validRegex = /^[0-9]{10}$/;
     if (mobile.value.match(validRegex)) {
-        // alert("Valid email address!");
-        // document.form1.text1.focus();
         return true;
     } else {
-        // alert("Invalid email address!");
-        // document.form1.text1.focus();
         return false;
     }
 }
